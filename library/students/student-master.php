00cb0824b8c4917919a834a3dd50126f0b7e3ed1
weish1995 <1607074855@qq.com>
2017-01-11 17:42:40 +0800

    master
    模板页左边菜单栏一部分

---
+++ library/students/student-master.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/master.css">
+</head>
+<body>
+	<header class="header">
+		<a class="header-logo" href="stu-index.php">Library</a>
+		<div class="header-person">
+			<img class="header-person-img" src="../imgs/header.png">
+			<div class="header-person-info">
+				<div class="header-person-info-name">Hello, 昵好</div>
+				<div class="header-person-info-role">student</div>
+			</div>
+		</div>
+		<nav>
+			<ul class="header-menu">
+				<li class="header-menu-cate click">
+					<a href="#">
+						<i class="header-menu-icon"></i>
+						<span>Dashboard</span>
+						<i class="header-menu-toggle"></i>
+					</a>
+					<ul>
+						<li>
+							<a href="#">
+								<i class="header-menu-icon"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="header-menu-icon"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="header-menu-icon"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="header-menu-icon"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="header-menu-icon"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li class="header-menu-cate">
+					<a href="#">
+						<i class="header-menu-icon"></i>
+						<span>Dashboard</span>
+						<i class="header-menu-toggle"></i>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</header> <!-- .header结束 侧边导航栏 -->
+</body>
+</html>
